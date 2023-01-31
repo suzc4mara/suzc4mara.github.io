e98a6a04152233360054f084b40787f711f25014
Luís Dante <luiscamaradev@gmail.com>
2023-01-31 19:25:45 -0300

    responsive navbar

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ const body = document.querySelector("body");
 themeToggler.addEventListener("change", () => {
   body.classList.toggle("light-theme");
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("on");
+  navList.classList.toggle("display");
+});
