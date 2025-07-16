3f7d21d15b0d5e7020c2721080f9a2368657ce17
Suzannie Câmara <suzanniecamara19@gmail.com>
2025-07-16 14:33:01 -0300

    Responsividade e logo adicionadas

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import PortfolioAnimation from "./PortfolioAnimation";
 AOS.init();
 PortfolioAnimation();
 
+
 const themeToggler = document.querySelector("#theme");
 const body = document.querySelector("body");
+const logoImg = document.getElementById("logo-img");
+
+function updateLogo() {
+  if (body.classList.contains("light-theme")) {
+    logoImg.src = "./assets/svg/logo2-header.svg";
+  } else {
+    logoImg.src = "./assets/svg/logo1-header.svg";
+  }
+}
 
 themeToggler.addEventListener("change", () => {
   body.classList.toggle("light-theme");
+  updateLogo();
 });
+
+// Atualiza logo ao carregar
+updateLogo();
 
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
